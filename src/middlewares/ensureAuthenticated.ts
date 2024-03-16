bd09084a8fe65002811c43ba2fd982093eb2d359
S0lux <loctranphuoc456@protonmail.com>
2024-03-16 22:11:43 +0700

    Added check to make sure user email is verified

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -5,11 +5,22 @@ const ensureAuthenticated = (
   res: Response,
   next: NextFunction
 ) => {
-  if (req.user) return next();
+  if (!req.user) {
+    return res.status(401).json({
+      error: { code: "UNAUTHORIZED", message: "You are not logged in." },
+    });
+  }
 
-  res.status(401).json({
-    error: { code: "UNAUTHORIZED", message: "You are not logged in" },
-  });
+  if (req.user && !req.user.emailVerified) {
+    return res.status(401).json({
+      error: {
+        code: "EMAIL_NOT_VERIFIED",
+        message: "You need to verify your email address.",
+      },
+    });
+  }
+
+  next();
 };
 
 export default ensureAuthenticated;
