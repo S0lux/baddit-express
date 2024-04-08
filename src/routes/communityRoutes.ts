fffcac0db2f2c820c5dbd02da348ccdb55f4922c
S0lux <loctranphuoc456@protonmail.com>
2024-04-08 09:37:28 +0700

    Minor refactoring

--- src/routes/communityRoutes.ts
+++ src/routes/communityRoutes.ts
@@ -2,16 +2,14 @@ import express from "express";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 import { communityController } from "../controllers/communityController";
 import { communityValidators } from "../validators/communityValidators";
+
 const router = express.Router();
 
-router.post(
-  "/",
-  communityValidators.create,
-  ensureAuthenticated,
-  communityController.createCommunity
-);
-router.post("/:communityName/member", ensureAuthenticated, communityController.joinCommunity);
-
 router.get("/:communityName", communityController.getCommunity);
+
+router.use(ensureAuthenticated);
+
+router.post("/", communityValidators.create, communityController.createCommunity);
+router.post("/:communityName/member", communityController.joinCommunity);
 
 export default router;
