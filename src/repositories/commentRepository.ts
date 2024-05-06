7c8445d7734c3f5087e9423e15457c6b4b2d4f4c
raccooncancode <ngoducloc14@gmail.com>
2024-05-06 14:25:50 +0700

    Added Cursor And Include Comment Vote To Get Comments With Queries

--- src/repositories/commentRepository.ts
+++ src/repositories/commentRepository.ts
@@ -1,5 +1,4 @@
 import { PostType, Prisma, PrismaClient, VoteState } from "@prisma/client";
-import { DefaultArgs } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
 
@@ -32,20 +31,40 @@ const getCommentsWithQueries = async (queries: {
 }) => {
   const rootComments = await prisma.comment.findMany({
     where: {
+      author: { id: queries.authorId },
       postId: queries.postId,
       parentId: null,
       deleted: false,
     },
+    take: 10,
+    skip: queries.cursor ? 1 : 0,
+    cursor: queries.cursor ? { id: queries.cursor } : undefined,
+    orderBy: { createdAt: "desc" },
     include: {
       children: true,
+      CommentVote: {
+        where: queries.requesterId
+          ? { user: { id: queries.requesterId } }
+          : { user: { id: "dummy-id" } },
+        select: {
+          state: true,
+          userId: false,
+          commentId: false,
+        },
+      },
+      author: {
+        select: {
+          avatarUrl: true,
+        },
+      },
     },
   });
-  await getNestedCommentsRecursively(rootComments);
+  await getNestedCommentsRecursively(rootComments, queries.requesterId);
   return rootComments;
 };
 
 //Add-on function
-async function getNestedCommentsRecursively(comments: IComment[]) {
+async function getNestedCommentsRecursively(comments: IComment[], requesterId?: string) {
   for (const comment of comments) {
     // Tìm các comment con của comment hiện tại
     const nestedComments = await prisma.comment.findMany({
@@ -55,6 +74,19 @@ async function getNestedCommentsRecursively(comments: IComment[]) {
       },
       include: {
         children: true,
+        CommentVote: {
+          where: requesterId ? { user: { id: requesterId } } : { user: { id: "dummy-id" } },
+          select: {
+            state: true,
+            userId: false,
+            commentId: false,
+          },
+        },
+        author: {
+          select: {
+            avatarUrl: true,
+          },
+        },
       },
     });
 
